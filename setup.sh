b4a7f244dae7ddaa93dba9331f6ab8c0fefab934
Sil van Diepen <me@silvandiepen.com>
2018-12-13 16:24:41 +0100

    fixed links and added colors

--- setup.sh
+++ setup.sh
@@ -7,26 +7,34 @@ printf 'App.scss created'
 # tools
 mkdir tools 
 
-curl -o tools/_index.scss -l ${URL}/tools-_index.scss 
-curl -o tools/_pre.scss -l ${URL}/tools-_pre.scss 
-curl -o tools/_post.scss -l ${URL}/tools-_post.scss 
+curl -o tools/_index.scss -l ${URL}/tools/_index.scss 
+curl -o tools/_pre.scss -l ${URL}/tools/_pre.scss 
+curl -o tools/_post.scss -l ${URL}/tools/_post.scss 
 
 printf 'Tools created!' 
 
 # typography
 mkdir typography 
 
-curl -o typography/_index.scss -l ${URL}/typography-_index.scss 
-curl -o typography/_embed.scss -l ${URL}/typography-_embed.scss 
-curl -o typography/_spacing.scss -l ${URL}/typography-_spacing.scss 
-curl -o typography/_usage.scss -l ${URL}/typography-_usage.scss 
+curl -o typography/_index.scss -l ${URL}/typography/_index.scss 
+curl -o typography/_embed.scss -l ${URL}/typography/_embed.scss 
+curl -o typography/_spacing.scss -l ${URL}/typography/_spacing.scss 
+curl -o typography/_usage.scss -l ${URL}/typography/_usage.scss 
 
 printf 'Typography created!' 
 
 #components
 mkdir components 
 
-curl -o components/_index.scss -l ${URL}/components-_index.scss 
-curl -o components/_example.scss -l ${URL}/components-_example.scss 
+curl -o components/_index.scss -l ${URL}/components/_index.scss 
+curl -o components/_example.scss -l ${URL}/components/_example.scss 
 
 printf 'Components created!'
+
+#colors
+mkdir color 
+
+curl -o color/_index.scss -l ${URL}/color/_index.scss 
+curl -o color/_custom-colors.scss -l ${URL}/color/_custom-colors.scss 
+
+printf 'Colors created!'
